e7a87d5045ae4a44083cd37fb8fbba0889fc909f
Franciszek Madej <f.madej@protonmail.com>
2019-11-15 00:19:22 +0100

    chore: remove accidentaly commited compiled JS

--- server/dist/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TemplateResolver_1 = require("resolvers/TemplateResolver");
-var resolvers = [TemplateResolver_1.default];
-exports.default = resolvers;
